e9fa55b19426559b4af53cebf5dcdb46d2621c44
Sean Seltzer <sseltzer431@gmail.com>
2017-01-29 13:30:15 -0500

    Reorganized project.

--- scripts/install.sh
+++
@@ -1,7 +0,0 @@
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
-nvm install node
-nvm use node
-
-npm install -g bower
-npm install -g gulp
-npm install -g bower
